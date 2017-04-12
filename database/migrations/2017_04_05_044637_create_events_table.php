5d9b2b23ba59fda2155203012ca78955fa117bf5
serg <aspirin_1988@mail.ru>
2017-04-12 09:09:23 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	public/access.log

--- database/migrations/2017_04_05_044637_create_events_table.php
+++ database/migrations/2017_04_05_044637_create_events_table.php
@@ -15,14 +15,14 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->default(null);
+            $table->string('name')->nullable();
             $table->text('content');
             $table->integer('city')->default(0);
-            $table->string('address');
+            $table->string('address')->nullable();
             $table->integer('event_type')->default(0);
-            $table->date('the_date');
-            $table->date('registration_date');
-            $table->string('link')->default(null);
+            $table->date('the_date')->nullable();
+            $table->date('registration_date')->nullable();
+            $table->string('link')->nullable();
             $table->string('user_id')->default(0);
             $table->timestamps();
         });
--- database/migrations/2017_04_07_083824_create_cities_table.php
+++ database/migrations/2017_04_07_083824_create_cities_table.php
@@ -15,7 +15,7 @@ class CreateCitiesTable extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->default(null);
+            $table->string('name')->nullable();
             $table->timestamps();
         });
     }
